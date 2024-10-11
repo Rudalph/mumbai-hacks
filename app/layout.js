4d0e51acb8de0031860d4e8756f2f75287508d8b
Rudalph <gonsalvesrudalph@gmail.com>
2024-10-11 21:29:50 +0530

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Poppins } from "next/font/google"
+
+const inter = Poppins({ subsets: ["latin"], weight: ['400', '500', '600', '700'] });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,7 +24,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
         {children}
       </body>
